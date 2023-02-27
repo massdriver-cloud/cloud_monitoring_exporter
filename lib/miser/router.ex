eea4eb132b458e0c07dbf55688e238f9e1f9b620
Andreas <andreas@massdriver.cloud>
2023-02-27 20:36:20 +0000

    Tryna get git push working

--- lib/miser/router.ex
+++ lib/miser/router.ex
@@ -7,6 +7,10 @@ defmodule Miser.Router do
 
   forward("/metrics", to: Miser)
 
+  match _ do
+    send_resp(conn, 404, "Oops!")
+  end
+
   def handle_errors(conn, %{kind: _kind, reason: _reason, stack: _stack}) do
     send_resp(conn, conn.status, "Something went wrong")
   end
