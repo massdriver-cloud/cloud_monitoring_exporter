77cdc263c915c350c8b606c71dd4488bc8e49f8b
Andreas <andreas@massdriver.cloud>
2023-03-01 01:40:46 +0000

    Now working!

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,11 +1,5 @@
 import Config
 
-if config_env() == :prod do
-  config :miser, credentials_json: File.read!("credentials.json")
-else
-  config :miser, credentials_json: File.read!("credentials_test.json")
-end
-
 config :miser, Miser,
   project_id: "md-sandbox-andreas",
   metric_type_prefixes: [
